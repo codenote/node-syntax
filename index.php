130a24595d505e6502e654d6d4a9c6e992aa6e50
yangxiaohao <admin@yangxiaohao.com>
2014-05-27 00:27:22 +0800

    initial

---
+++ index.php
@@ -0,0 +1,60 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>NodeSyntax</title>
+<style>
+pre {
+	font-family: Consolas, "Liberation Mono", Courier, monospace;
+	line-height: 18px;
+	padding:0px;
+	margin:16px;
+	tab-size:4;
+	color: #000000;
+	font-size:12px;
+	font-weight: bold;
+}
+
+.c_v {
+	color:#008080;
+}
+.c_n {
+	color:#FF0000;
+}
+.c_k {
+	color:#0000FF;
+}
+.c_s {
+	color: #A00000;
+}
+.c_f {
+	color: #268FAD;
+}
+.c_x {
+	color: #006FE0;
+}
+.c_c {
+	color: #008000;
+}
+.c_d {
+	color:#800080;
+}
+</style>
+</head>
+<body>
+
+<pre><?php
+require('node-syntax.php');
+$content = file_get_contents('samples/php.txt');
+$content = htmlspecialchars($content);
+echo preg_replace('/\r\n/u', '<br />', $content);
+?></pre>
+<pre><?php
+$content = NodeSyntax::highlight($content);
+
+echo preg_replace('/\r\n/u', '<br />', $content);
+?></pre>
+
+</body>
+</html>
+
---
+++ node-syntax.php
@@ -0,0 +1,31 @@
+﻿<?php
+/*
+i: PCRE_CASELESS
+m: PCRE_MULTILINE
+	^ and $ fit both \r and \n
+s: PCRE_DOTALL
+	. fit also fit \r \n
+
+*/
+
+
+
+class NodeSyntax {
+	public static function highlight($s) {
+		$s = preg_replace_callback('/(\/\/.*?(?:\R|$)|\/\*.*?\*\/|&quot;.*?(?<!\\\\)(?:\\\\\\\\)*&quot;|\'.*?(?<!\\\\)(?:\\\\\\\\)*\')/s','self::_callback_debug', $s);
+		return $s;
+	}
+	
+	static function _callback_debug( $matches ) {
+		//print_r($matches);
+		
+		$token = $matches[0];
+		if (strlen($token) == 0) return '';
+		
+		if( $token[0] == '&' || $token[0] == "'" ) {
+			return '<span class="c_s">'.$token.'</span>';
+		} else {
+			return '<span class="c_c">'.$token.'</span>';
+		}
+	}
+}
