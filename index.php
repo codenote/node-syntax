fbbd96207bddddcb4c36a1c0c6858781740f8a52
yangxiaohao <admin@yangxiaohao.com>
2014-05-27 16:40:22 +0800

    finish most of c and php

--- index.php
+++ index.php
@@ -19,13 +19,13 @@ pre {
 	color:#008080;
 }
 .c_n {
-	color:#FF0000;
+	color:#A70000;
 }
 .c_k {
 	color:#0000FF;
 }
 .c_s {
-	color: #A00000;
+	color: #A70000;
 }
 .c_f {
 	color: #268FAD;
@@ -36,25 +36,39 @@ pre {
 .c_c {
 	color: #008000;
 }
-.c_d {
-	color:#800080;
+.c_t {
+	color:#FF0000;
+}
+.c_o {
+	color:#002D7A;
 }
 </style>
 </head>
 <body>
-
-<pre><?php
+<?php
 require('node-syntax.php');
+?>
+<pre><?php
+$content = file_get_contents('samples/general.txt');
+$content = htmlspecialchars($content);
+$nodesyntax = new NodeSyntax();
+$content = $nodesyntax->highlight($content);
+echo preg_replace('/\r\n/u', '<br />', $content);
+?></pre>
+<pre><?php
+$content = file_get_contents('samples/c.txt');
+$content = htmlspecialchars($content);
+$nodesyntax = new NodeSyntax();
+$content = $nodesyntax->highlight($content, 'c');
+echo preg_replace('/\r\n/u', '<br />', $content);
+?></pre>
+<pre><?php
 $content = file_get_contents('samples/php.txt');
 $content = htmlspecialchars($content);
+$nodesyntax = new NodeSyntax();
+$content = $nodesyntax->highlight($content, 'php');
 echo preg_replace('/\r\n/u', '<br />', $content);
 ?></pre>
-<pre><?php
-$content = NodeSyntax::highlight($content);
-
-echo preg_replace('/\r\n/u', '<br />', $content);
-?></pre>
-
 </body>
 </html>
 
--- node-syntax.php
+++ node-syntax.php
@@ -11,44 +11,107 @@ s: PCRE_DOTALL
 
 
 class NodeSyntax {
-	static $pregs = array (
-			'\/\/.*?(?:\R|$)',
-			'\/\*.*?\*\/',
-			'&quot;.*?(?<!\\\\)(?:\\\\\\\\)*&quot;',
-			'\'.*?(?<!\\\\)(?:\\\\\\\\)*\'',
-		);
-	static $types = array (
-			'c_c',
-			'c_c',
-			'c_s',
-			'c_s',
-		);
+
+	private $list_preg = [];
+	private $list_type = [];
+	private $list_style = [];
+	private $token_last;
+	private $type_last;
 	
-	static function highlight($s) {
+	function highlight($string, $language = null) {
+		$callback = '_callback_general';
 		
 		
+		if ($language == null) {
+			$this->prepare_preg('comment', 'comment_1');
+			$this->prepare_preg('comment', 'comment_2');
+			$this->prepare_preg('comment', 'comment_3');
+			$this->prepare_preg('string', 'string_1');
+			$this->prepare_preg('string', 'string_2');
+			$this->prepare_preg('tag', 'tag_1');
+			$this->prepare_preg('xml', 'xml_1');
+			$this->prepare_preg('operator', 'operator_1');
+			$this->prepare_preg('number', 'number_1');
+			$this->prepare_preg('keyword', 'keyword_1');
+			$this->prepare_preg('keyword', 'keyword_2');
+			$this->prepare_preg('variable', 'variable_1');
+			$this->prepare_preg('macro', 'macro_1');
+			
+		} else if ($language == 'c') {
+			$this->prepare_preg('comment', 'comment_1');
+			$this->prepare_preg('comment', 'comment_2');
+			$this->prepare_preg('string', 'string_1');
+			$this->prepare_preg('string', 'string_2');
+			$this->prepare_preg('operator', 'operator_2');
+			$this->prepare_preg('operator', 'operator_3');
+			$this->prepare_preg('number', 'number_1');
+			$this->prepare_preg('keyword', 'keyword_1');
+			$this->prepare_preg('macro', 'macro_1');
+		} else if ($language == 'php') {
+			$this->prepare_preg('comment', 'comment_1');
+			$this->prepare_preg('comment', 'comment_2');
+			$this->prepare_preg('string', 'string_1');
+			$this->prepare_preg('string', 'string_2');
+			$this->prepare_preg('tag', 'tag_1');
+			$this->prepare_preg('operator', 'operator_4');
+			$this->prepare_preg('operator', 'operator_2');
+			$this->prepare_preg('operator', 'operator_3');
+			$this->prepare_preg('number', 'number_1');
+			$this->prepare_preg('keyword', 'keyword_1');
+			$this->prepare_preg('keyword', 'keyword_2');
+			$this->prepare_preg('variable', 'variable_1');
+		}
 		
-		$rgex = implode(')|(', self::$pregs);
-		$rgex = '/('.$rgex.')/s';
-		$s = preg_replace_callback($rgex,'self::_callback_debug', $s);
-		//$s = preg_replace_callback('/(\/\/.*?(?:\R|$)|\/\*.*?\*\/|&quot;.*?(?<!\\\\)(?:\\\\\\\\)*&quot;|\'.*?(?<!\\\\)(?:\\\\\\\\)*\')/s','self::_callback_debug', $s);
-		return $s;
+		$regex = implode(')|(', $this->list_preg);
+		$regex = '/('.$regex.')/s';
+		// echo htmlspecialchars($regex);
+		$string = preg_replace_callback($regex, array( &$this, $callback), $string);
+		return $string;
 	}
 	
-	static function _callback_debug( $matches ) {
+	private static $lib_grep = array (
+		'comment_1' => '\/\/.*?(?:\R|$)',
+		'comment_2' => '\/\*.*?\*\/',
+		'comment_3' => '\&lt\;!--.*--\&gt\;',
+		'string_1' => '&quot;.*?(?<!\\\\)(?:\\\\\\\\)*&quot;',
+		'string_2' => '\'.*?(?<!\\\\)(?:\\\\\\\\)*\'',
+		'operator_1' => '&gt;&gt;\=|&lt;&lt;\=|\+\=|\-\=|\*\=|\/\=|%\=|&\=|\^\=||\=|&gt;&gt;|&lt;&lt;|\+\+|\-\-|\-&gt;|&amp;&amp;|\|\||&lt;\=|&gt;\=|\=\=|!\=|;|\{|&lt;%|\}|%&gt;|,|:|\=|\(|\)|\[|&lt;:|\]|:&gt;|\.|&amp;|!|~|\-|\+|\*|\/|%|&lt;|&gt;|\^|\||\?',
+		'operator_2' => '&gt;&gt;\=|&lt;&lt;\=|\+\=|\-\=|\*\=|\/\=|%\=|&\=|\^\=||\=|&gt;&gt;|&lt;&lt;|\+\+|\-\-|&amp;&amp;|\|\||&lt;\=|&gt;\=|\=\=|!\=|\=|&amp;|!|~|\-|\+|\*|\/|%|&lt;|&gt;|\^|\|',
+		'operator_3' => '\{|\}|\(|\)|\[|\]|;',
+		'operator_4' => '\?\|::|:|\.|\-&gt;|\=&gt;',
+		'number_1' => '(?<!\w)0x[\da-f]+|\d+(?!\w)',
+		'token_1' => '(?<![\w\d])[a-zA-Z_]+\w*(?!\w)',
+		'variable_1' => '(?<!\w)(?:\$)(?:\w)+(?!\w)',
+		'macro_1' => '(?<!\w)(?:\#)(?:\w)+(?!\w)',
+		'keyword_1' => '(?<![\w\d])(?:auto|break|case|char|const|continue|default|do|double|else|enum|extern|float|for|goto|if|inline|int|long|register|restrict|return|short|signed|sizeof|static|struct|switch|typedef|union|unsigned|void|volatile|while|NULL)(?!\w)',
+		'keyword_2' => '(?<![\w\d])(?:public|private|class|self|function|null|foreach|as|new|TRUE|true|FALSE|false)(?!\w)',
+		'tag_1' => '(?<!\w)(?:(?:(?:&lt;\?)(?:php|asp)?)|(?:\?&gt;))(?!\w)',
+		'xml_1' => '\&lt\;[\/]?[a-zA-Z_]+[a-zA-Z0-9_]*[ ]*[\/]?\&gt\;',
+	);
 	
-		
-		// print_r($matches);
+	private static $lib_style = array (
+		'comment' => 'c_c',
+		'string' => 'c_s',
+		'operator' => 'c_o',
+		'number' => 'c_n',
+		'variable' => 'c_v',
+		'keyword' => 'c_k',
+		'function' => 'c_f',
+		'macro' => 'c_k',
+		'tag' => 'c_t',
+		'xml' => 'c_x',
+	);
+	
+	private function prepare_preg($type, $grep) {
+		array_push($this->list_preg, self::$lib_grep[$grep]);
+		array_push($this->list_style, self::$lib_style[$type]);
+		array_push($this->list_type, $type);
+	}
+	
+	private function _callback_general( $matches ) {
 		$index = count($matches)-2;
 		$token = $matches[0];
 		if (strlen($token) == 0) return '';
-		return '<span class="'.self::$types[$index].'">'.$token.'</span>';
-		/*
-		if( $token[0] == '&' || $token[0] == "'" ) {
-			return '<span class="c_s">'.$token.'</span>';
-		} else {
-			return '<span class="c_c">'.$token.'</span>';
-		}
-		*/
+		return '<span class="'.$this->list_style[$index].'">'.$token.'</span>';
 	}
 }
